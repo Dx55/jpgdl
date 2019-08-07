62c07bb091fc0802700a630aab7a58d2a3ae0d25
Dx55 <45577743+Dx55@users.noreply.github.com>
2019-08-07 11:22:52 +0200

    Add files via upload

---
+++ imagetopdf.py
@@ -0,0 +1,17 @@
+#By Dx55
+
+from PIL import Image
+
+#You can add more than one image without problem
+
+im1 = Image.open("Image path")
+im2 = Image.open("Image path")
+
+#Creating the PDF file list of image (also here you can add more image)
+im_list = [im2]
+
+#Putting PDF file path
+pdf1_filename = "final pdf file path"
+
+#Saving PDF file
+im1.save(pdf1_filename, "PDF" ,resolution=100.0, save_all=True, append_images=im_list)
