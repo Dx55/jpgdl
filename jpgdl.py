c5300ffd2f6cb4eeb4675296a577662ea20515e3
Dx55 <45577743+Dx55@users.noreply.github.com>
2019-08-09 18:34:36 +0200

    Update jpgdl.py

--- jpgdl.py
+++ jpgdl.py
@@ -3,12 +3,18 @@
 import requests
 import shutil
 
-x = str(input("Link\n"))
-y = str(input("Name\n"))
-
-r = requests.get(x, stream=True, headers={'User-agent': 'Mozilla/5.0'})
-if r.status_code == 200:
-    with open(str(y + ".jpg"), 'wb') as f:
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)
+#Download function
+def download():
+        x = str(input("Link\n"))
+        y = str(input("Name\n"))
         
+        r = requests.get(x, stream=True, headers={'User-agent': 'Mozilla/5.0'})
+        if r.status_code == 200:
+            with open(str(y + ".jpg"), 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+#Loop
+while True:
+    download()
+    if input("Un'altra?") == 'n':
+        break
